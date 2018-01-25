8e86bc80f581e0eb1d05d00de125fd03caab3cb2
Ilia Kopylov <iliakplv@gmail.com>
2018-01-25 17:10:42 +1300

    Add more todos

--- nlp/text_preprocessing.py
+++ nlp/text_preprocessing.py
@@ -1,10 +1,19 @@
 # TODO (preprocessing)
+# remove tags, numbers, punctuation
+# lowercase
 # stop-words
 # spelling mistakes
 # accents
 # stemming
 
 # TODO (features)
+# Vector space model
 # TF-IDF
 # number of words/paragraphs
 # number of spelling mistakes
+# hashing trick
+
+# TODO (ML)
+# Naive Bayes
+# SVM
+# Grid search
